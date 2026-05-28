49004391c46cd1da775177bbca0d290d87fbcdec
jhoel0521 <jhoel0521@gmail.com>
2026-05-28 09:29:56 -0400

    feat: mejorar la validación de entrada en la creación de categorías

--- secciones/categorias/crear.php
+++ secciones/categorias/crear.php
@@ -1,7 +1,7 @@
 <?php require_once __DIR__ . '/../../bd.php';
-if ($_POST) {
+if (request()->method() === 'POST') {
     //Validacion del nombre categoria
-    $category_name = (isset($_POST['category_name']) ? $_POST['category_name'] : "");
+    $category_name = request()->get('category_name', "");
     //Preparar la insercion de datos
     \DB::ejecutarConsulta(
         "INSERT INTO categories (category_id, category_name) VALUES (NULL, :category_name)",
