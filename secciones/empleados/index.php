488417d3bc0d2cd3b995896ed452a06ee7557b67
jhoel0521 <jhoel0521@gmail.com>
2026-05-29 20:44:35 -0400

    feat: eliminar secciones de empleados, incluyendo archivos de creación, edición e índice

--- secciones/empleados/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/../../libs/functions.php';
-ensureSeccion();
-app_require_auth();
-include __DIR__ . '/../../templates/header.php';
-?>
-
-<?php include("../../templates/footer.php"); ?>
